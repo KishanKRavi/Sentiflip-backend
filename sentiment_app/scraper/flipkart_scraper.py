26840a24ab8d7f4dc6228cec2a3f39359cc6352e
Kishan KR <kishankr0204@gmail.com>
2025-05-23 20:56:25 +0530

    commit done

--- sentiment_app/scraper/flipkart_scraper.py
+++ sentiment_app/scraper/flipkart_scraper.py
@@ -134,11 +134,9 @@ def configure_driver():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    )
-    return uc.Chrome(options=options)
+
+    # DO NOT set binary_location manually
+    return uc.Chrome(options=options, headless=True)
 
 def search_product(query):
     driver = configure_driver()
