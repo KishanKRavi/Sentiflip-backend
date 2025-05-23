01dc14e0ce94fbcb76ec3f72fded4a4f78ca8103
Kishan KR <kishankr0204@gmail.com>
2025-05-23 20:32:02 +0530

    edit made

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Install Google Chrome
-echo "Installing Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt update && apt install -y ./google-chrome-stable_current_amd64.deb
+echo "Installing Google Chrome..."
 
-# Install ChromeDriver
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update || true
+apt-get install -y ./google-chrome-stable_current_amd64.deb || true
+
 echo "Installing ChromeDriver..."
-CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f 3 | cut -d '.' -f 1)
-wget https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0.0/chromedriver_linux64.zip
+
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+' | head -1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 mv chromedriver /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
